65799c486a7319922611e0477e2d3d06fb12455c
Flavia de Almeida <flaviatait@gmail.com>
2022-07-21 21:56:24 -0300

    Treinando divisão

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let numPar = Number (prompt("Insira um número par"));
+const num = 2;
+console.log('O resto da divisão é', numPar%num);
+
+// O resto da divisão sempre será 0
+// Ao inserir um número ímpar o resto sempre será 1
